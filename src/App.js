6b564a2aadc4179e82e1166ea6f83fbd3b3fb98c
dynamicbalaji <balajiashokkumar@yahoo.com>
2019-09-01 21:50:57 +0530

    Adding a search field with state & onchange event

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -21,6 +22,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input type="search" placeholder="search monster" onChange={(e) => {
+          this.setState({searchField: e.target.value}, () => {
+            console.log(this.state.searchField);
+          });
+          }} />
         <CardList monsters={this.state.monsters} />
       </div>
     );
