a315dfc117ae20360128c41dc0c2edbe886a4bf9
dynamicbalaji <balajiashokkumar@yahoo.com>
2019-09-01 21:52:08 +0530

    Formatted using prettier extension

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import CardList from './components/CardList/CardList';
+import CardList from "./components/CardList/CardList";
 
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: ""
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(users => this.setState({ monsters: users }))
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        <input type="search" placeholder="search monster" onChange={(e) => {
-          this.setState({searchField: e.target.value}, () => {
-            console.log(this.state.searchField);
-          });
-          }} />
+        <input
+          type="search"
+          placeholder="search monster"
+          onChange={e => {
+            this.setState({ searchField: e.target.value }, () => {
+              console.log(this.state.searchField);
+            });
+          }}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
