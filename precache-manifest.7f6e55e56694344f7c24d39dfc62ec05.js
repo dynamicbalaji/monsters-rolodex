41af6a0807d6e1c8a4fceb92e144f829b260fcff
dynamicbalaji <balajiashokkumar@yahoo.com>
2019-09-02 00:16:19 +0530

    Updates

---
+++ precache-manifest.7f6e55e56694344f7c24d39dfc62ec05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e39acf719adae9644773ed923cbdd156",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "24c5c2596acde18fac95",
+    "url": "/monsters-rolodex/static/css/main.c6d97a1f.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "24c5c2596acde18fac95",
+    "url": "/monsters-rolodex/static/js/main.4f18520b.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
