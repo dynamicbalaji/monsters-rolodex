dbd77275b92f57452a9ddc4ada6f90705b495a9c
dynamicbalaji <balajiashokkumar@yahoo.com>
2019-09-01 14:29:14 +0530

    Adding emails in p tag below name

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,6 +6,7 @@ const Card = (props) => {
     return (<div className="card-container">
         <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster" />
         <h2>{props.monster.name}</h2>
+        <p>{props.monster.email}</p>
     </div>);
 }
 
