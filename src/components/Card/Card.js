abaa96929a02812e4020cc3715e939ea967d55ef
dynamicbalaji <balajiashokkumar@yahoo.com>
2019-09-01 14:27:50 +0530

    *Adding images for each cards using robohash link
    <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster" />
    Use $ while including js expressions inside ``

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,6 +4,7 @@ import './Card.css';
 
 const Card = (props) => {
     return (<div className="card-container">
+        <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster" />
         <h1>{props.monster.name}</h1>
     </div>);
 }
